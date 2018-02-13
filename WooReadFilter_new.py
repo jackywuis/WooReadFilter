d9219f31ddfdfc76a0a13b5f53a31c6b85005544
Jacky Woo <35942959+jackywuis@users.noreply.github.com>
2018-02-13 17:02:12 +0800

    Update WooReadFilter_new.py

--- WooReadFilter_new.py
+++ WooReadFilter_new.py
@@ -16,24 +16,25 @@ args = parser.parse_args()
 print 'Thank you for use WooRead! You set input %s, %s and output %s' % (
     args.input_file_1, args.input_file_2, args.output_file)
 main_dict = [[], [], []]
-for count, seq in enumerate([args.input_file_1, args.input_file_2]):
-    seqfile = open(seq)
-    for read in seqfile:
-        if not count:
-            main_dict[0] += [read.name.split(' ')[0]]
-            main_dict[1] += [read]
-            main_dict[2] += [None]
-        elif read.name.split(' ')[0] in main_dict[0]:
-            main_dict[2][main_dict[0].index(read.name.split(' ')[0])] = read
-    seqfile.close()
+seqfile = open(args.input_file_1)
+for read in seqfile:
+    main_dict[0] += [read.name.split(' ')[0]]
+    main_dict[1] += [read]
+    main_dict[2] += [None] 
+seqfile.close()
+seqfile = open(args.input_file_2)
+for read in seqfile:
+    if read.name.split(' ')[0] in main_dict[0]:
+        main_dict[2][main_dict[0].index(read.name.split(' ')[0])] = read
+seqfile.close()
+print 'Reading raw data finished. Now outputing...'
 outfh_1 = open(args.output_file + '1.fastq', 'w')
 outfh_2 = open(args.output_file + '2.fastq', 'w')
 for co, item in enumerate(main_dict[2]):
     if item:
         for count, counter in enumerate([outfh_1, outfh_2]):
-            p = '@' + main_dict[count + 1][co]['name'].split('#')[0] + '\n' + main_dict[count + 1][co]['sequence'] + \
-                '\n+\n' + main_dict[count + 1][co]['quality'] + '\n'
-            counter.write(p)
+            counter.write('@' + main_dict[count + 1][co]['name'].split('#')[0] + '\n' + main_dict[count + 1][co]['sequence'] + \
+                '\n+\n' + main_dict[count + 1][co]['quality'] + '\n')
 outfh_1.close()
 outfh_2.close()
 print 'WooRead finished!'
